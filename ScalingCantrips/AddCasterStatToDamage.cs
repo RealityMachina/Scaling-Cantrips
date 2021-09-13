eeaab8b34dd16b7c249537053368dfa765a99e2a
RealityMachina <tomclancy1995@gmail.com>
2021-09-12 22:15:45 -0400

    Added Cantrip Expert feats.

---
+++ ScalingCantrips/AddCasterStatToDamage.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.EntitySystem.Stats;
+using Kingmaker.PubSubSystem;
+using Kingmaker.RuleSystem.Rules.Damage;
+using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.UnitLogic.Mechanics;
+using Kingmaker.Utility;
+using UnityEngine;
+
+namespace ScalingCantrips
+{
+    [TypeId("a7f5a54170ec4a0aa88fa1638b16dc61")]
+    public class AddCasterStatToDamage : UnitFactComponentDelegate, IInitiatorRulebookHandler<RuleCalculateDamage>, IRulebookHandler<RuleCalculateDamage>, ISubscriber, IInitiatorRulebookSubscriber
+    {
+        // Token: 0x0600BD9F RID: 48543 RVA: 0x002F5C48 File Offset: 0x002F3E48
+        public void OnEventAboutToTrigger(RuleCalculateDamage evt)
+        {
+            MechanicsContext context = evt.Reason.Context;
+            if ((context?.SourceAbility) == null)
+            {
+                return;
+            }
+            if ((context?.MaybeCaster) == null)
+            {
+                return;
+            }
+            if ( (!context.SourceAbility.IsSpell && this.SpellsOnly) || context.SourceAbility.Type == AbilityType.Physical)
+            {
+                return;
+            }
+
+            if(!context.SourceAbility.IsCantrip && CantripsOnly)
+            {
+                return;
+            }
+
+            if(statType == 0 || (int)statType > 6)
+            {
+                return;
+            }
+            ModifiableValueAttributeStat CasterStat = context.MaybeCaster.Stats.GetAttribute(statType);  //if this is null somehow something has gone very wrong
+
+            if(CasterStat == null)
+            {
+                return;
+            }
+            foreach (BaseDamage baseDamage in evt.DamageBundle)
+            {
+                int bonus = CasterStat.Bonus;
+                baseDamage.AddModifier(bonus, base.Fact);
+            }
+        }
+
+        // Token: 0x0600BDA0 RID: 48544 RVA: 0x00003AD6 File Offset: 0x00001CD6
+        public void OnEventDidTrigger(RuleCalculateDamage evt)
+        {
+        }
+
+        
+        public StatType statType;
+
+        public bool SpellsOnly = true;
+
+        // Token: 0x04007BFF RID: 31743
+        public bool UseContextBonus;
+
+        public bool CantripsOnly = true;
+
+        // Token: 0x04007C00 RID: 31744
+        [ShowIf("UseContextBonus")]
+        public ContextValue Value;
+    }
+}
---
+++ ScalingCantrips/CantripFeaturePatcher.cs
@@ -0,0 +1,156 @@
+﻿using HarmonyLib;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.Enums;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.UnitLogic.Abilities.Components;
+using Kingmaker.UnitLogic.Mechanics;
+using Kingmaker.UnitLogic.Mechanics.Actions;
+using Kingmaker.UnitLogic.Mechanics.Components;
+using ScalingCantrips.Extensions;
+using ScalingCantrips.Utilities;
+using ScalingCantrips.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
+using Kingmaker.Designers.Mechanics.Buffs;
+using Kingmaker.UnitLogic.Mechanics.Properties;
+using Kingmaker.UnitLogic;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Prerequisites;
+using Kingmaker.Blueprints.Classes.Selection;
+
+namespace ScalingCantrips
+{
+    class CantripFeaturePatcher
+    {
+        [HarmonyPatch(typeof(BlueprintsCache), "Init")]
+        public static class BlueprintPatcher
+        {
+            static bool Initialized;
+
+            static void Postfix()
+            {
+                if (Initialized) return;
+                Initialized = true;
+                Main.Log("Adding Cantrip Feats");
+               // CreateAddCasterStateToDamage();
+                AddCantripFeatures(); //int wis and cha
+            }
+
+            static void AddCantripFeatures()
+            {
+                var AddWisToCantripDamage = Helpers.CreateBlueprint<BlueprintFeature>("RMAddWisStatToDamage", bp => {
+                    bp.IsClassFeature = true;
+                    bp.Groups = new FeatureGroup[] {
+                    FeatureGroup.WizardFeat, FeatureGroup.Feat};
+                    bp.Ranks = 1;
+                    bp.SetName("Cantrip Expert (Wisdom)");
+                    bp.SetDescription("Cantrips you can cast now have a damage bonus equal to your Wisdom stat bonus.");
+                    bp.m_DescriptionShort = bp.m_Description;
+                    bp.AddComponent(Helpers.Create<AddCasterStatToDamage>(c => {
+                        c.statType = Kingmaker.EntitySystem.Stats.StatType.Wisdom;
+                       
+                    }));
+                    bp.AddComponent(Helpers.Create<PrerequisiteNoFeature>(c => {
+                        c.m_Feature = Resources.GetModBlueprint<BlueprintFeature>("RMAddIntStatToDamage").ToReference<BlueprintFeatureReference>();
+
+                    }));
+                    bp.AddComponent(Helpers.Create<PrerequisiteNoFeature>(c => {
+                        c.m_Feature = Resources.GetModBlueprint<BlueprintFeature>("RMAddChaStatToDamage").ToReference<BlueprintFeatureReference>();
+
+                    }));
+
+                });
+
+                var AddIntToCantripDamage = Helpers.CreateBlueprint<BlueprintFeature>("RMAddIntStatToDamage", bp => {
+                    bp.IsClassFeature = true;
+                    bp.Groups = new FeatureGroup[] {
+                    FeatureGroup.WizardFeat, FeatureGroup.Feat};
+                    bp.Ranks = 1;
+                    bp.SetName("Cantrip Expert (Intelligence)");
+                    bp.SetDescription("Cantrips you can cast now have a damage bonus equal to your Intelligence stat bonus.");
+                    bp.m_DescriptionShort = bp.m_Description;
+                    bp.AddComponent(Helpers.Create<AddCasterStatToDamage>(c => {
+                        c.statType = Kingmaker.EntitySystem.Stats.StatType.Intelligence;
+
+                    }));
+                    bp.AddComponent(Helpers.Create<PrerequisiteNoFeature>(c => {
+                        c.m_Feature = Resources.GetModBlueprint<BlueprintFeature>("RMAddWisStatToDamage").ToReference<BlueprintFeatureReference>();
+
+                    }));
+                    bp.AddComponent(Helpers.Create<PrerequisiteNoFeature>(c => {
+                        c.m_Feature = Resources.GetModBlueprint<BlueprintFeature>("RMAddChaStatToDamage").ToReference<BlueprintFeatureReference>();
+
+                    }));
+
+                });
+
+                var AddChaToCantripDamage = Helpers.CreateBlueprint<BlueprintFeature>("RMAddChaStatToDamage", bp => {
+                    bp.IsClassFeature = true;
+                    bp.Groups = new FeatureGroup[] {
+                    FeatureGroup.WizardFeat, FeatureGroup.Feat};
+                    bp.Ranks = 1;
+                    bp.SetName("Cantrip Expert (Charisma)");
+                    bp.SetDescription("Cantrips you can cast now have a damage bonus equal to your Charisma stat bonus.");
+                    bp.m_DescriptionShort = bp.m_Description;
+                    bp.AddComponent(Helpers.Create<AddCasterStatToDamage>(c => {
+                        c.statType = Kingmaker.EntitySystem.Stats.StatType.Intelligence;
+
+                    }));
+                    bp.AddComponent(Helpers.Create<PrerequisiteNoFeature>(c => {
+                        c.m_Feature = Resources.GetModBlueprint<BlueprintFeature>("RMAddWisStatToDamage").ToReference<BlueprintFeatureReference>();
+
+                    }));
+                    bp.AddComponent(Helpers.Create<PrerequisiteNoFeature>(c => {
+                        c.m_Feature = Resources.GetModBlueprint<BlueprintFeature>("RMAddIntStatToDamage").ToReference<BlueprintFeatureReference>();
+
+                    }));
+
+                });
+
+                AddFeaturetoSelection(AddWisToCantripDamage);
+                AddFeaturetoSelection(AddIntToCantripDamage);
+                AddFeaturetoSelection(AddChaToCantripDamage);
+            }
+
+            static void AddFeaturetoSelection(BlueprintFeature feat)
+            {
+
+                var ArcaneRiderFeat = Resources.GetBlueprint<BlueprintFeatureSelection>("8e627812dc034b9db12fa396fdc9ec75");
+                var BasicFeat = Resources.GetBlueprint<BlueprintFeatureSelection>("247a4068296e8be42890143f451b4b45");
+                var ArcaneBloodline = Resources.GetBlueprint<BlueprintFeatureSelection>("ff4fd877b4c801342ab8e880b734a6b9");
+                var InfernalBloodline = Resources.GetBlueprint<BlueprintFeatureSelection>("f19d9bfcbc1e3ea42bda754a03c40843");
+                var DragonLevel2Feat = Resources.GetBlueprint<BlueprintFeatureSelection>("a21acdafc0169f5488a9bd3256e2e65b");
+                var LoremasterFeat = Resources.GetBlueprint<BlueprintFeatureSelection>("689959eef3e972e458b52598dcc2c752");
+                var MagusFeat = Resources.GetBlueprint<BlueprintFeatureSelection>("66befe7b24c42dd458952e3c47c93563");
+                var SeekerFeat = Resources.GetBlueprint<BlueprintFeatureSelection>("c6b609279cc3174478624182ac1ad812");
+                var SkaldFeat = Resources.GetBlueprint<BlueprintFeatureSelection>("0a1999535b4f77b4d89f689a385e5ec9");
+                var WizardFeat = Resources.GetBlueprint<BlueprintFeatureSelection>("8e627812dc034b9db12fa396fdc9ec75");
+
+
+                AddtoSelection(feat, ArcaneRiderFeat);
+                AddtoSelection(feat, BasicFeat);
+                AddtoSelection(feat, ArcaneBloodline);
+                AddtoSelection(feat, InfernalBloodline);
+                AddtoSelection(feat, DragonLevel2Feat);
+                AddtoSelection(feat, LoremasterFeat);
+                AddtoSelection(feat, MagusFeat);
+                AddtoSelection(feat, SeekerFeat);
+                AddtoSelection(feat, SkaldFeat);
+                AddtoSelection(feat, WizardFeat);
+
+            }
+            static void AddtoSelection(BlueprintFeature feat, BlueprintFeatureSelection selection)
+            {
+
+                selection.m_Features = selection.m_AllFeatures.AppendToArray(feat.ToReference<BlueprintFeatureReference>());
+                selection.m_AllFeatures = selection.m_AllFeatures.AppendToArray(feat.ToReference<BlueprintFeatureReference>());
+
+            }
+        }
+    }
+}
