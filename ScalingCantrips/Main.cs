0a6f46d3e8c96b9c2719691c12c7156c5cfcde0a
RealityMachina <tomclancy1995@gmail.com>
2021-09-16 12:29:23 -0400

    starting to prepare for GUI-based settings

--- ScalingCantrips/Main.cs
+++ ScalingCantrips/Main.cs
@@ -50,7 +50,7 @@ namespace ScalingCantrips
             {
                 return;
             }
-
+            
         }
 
 
--- ScalingCantrips/Settings.cs
+++ ScalingCantrips/Settings.cs
@@ -9,7 +9,42 @@ namespace ScalingCantrips
         {
             UnityModManager.ModSettings.Save<Settings>(this, modEntry);
         }
-       
+
+
+
+        int CasterLevelsReq = 2;
+
+        int MaxDice = 6;
+
+
+        int DisruptCasterLevelsReq = 3;
+
+
+        int DisruptMaxDice = 4;
+
+     int VirtueCasterLevelsReq = 2;
+
+        int VirtueMaxDice = 10;
+
+
+        bool IgnoreDivineZap = false;
+
+
+        int JoltingGraspLevelsReq = 2;
+
+
+        int JoltingGraspMaxDice = 7;
+
+
+        int DisruptLifeLevelsReq = 2;
+
+
+        int DisruptLifeMaxDice = 6;
+
+        bool DontAddUnholyZap = false;
+
+
+        bool StartImmediately = true;
     }
 
 }
