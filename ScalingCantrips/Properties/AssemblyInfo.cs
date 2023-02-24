c004cf27530aea719449129913cc61511b1f2246
Zach Gerth <me@ladyradia.com>
2023-02-24 00:59:41 -0500

    fix beta 2.10 issues

--- ScalingCantrips/Properties/AssemblyInfo.cs
+++ ScalingCantrips/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyTitle("ScalingCantirps")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyTitle("ScalingCantrips")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -15,4 +15,4 @@ using System.Runtime.Versioning;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4dc448e8-2a06-415c-926f-aa5e6d3cd73b")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
